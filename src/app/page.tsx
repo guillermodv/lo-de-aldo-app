631a67ed3d560a96a6ffe00f805e35a7d03f7dfe
Guillermo <guillermo.dv@gmail.com>
2024-06-29 22:20:43 -0300

    fotos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
-
 import { useForm } from "react-hook-form";
+
 import WhatsappLogo from "../components/whatsappLogo";
 import { Label } from "../constants/label";
 
@@ -18,18 +18,18 @@ export default function Home() {
 
   const onSubmit = (data: any) => {
     openInNewTab(
-      `${Label.MESSAGE_DELIVERY} Pedido a nombre de:${data.firstName} Direccion: ${data.address} Cantidad de pizzas: ${data.quantity} `
+      `${Label.MESSAGE_DELIVERY} Pedido a nombre de: ${data.firstName} Direccion: ${data.address} Detalle: ${data.quantity} `
     );
   };
 
   return (
-    <main className="flex flex-col items-center justify-between pt-4 w:12/12">
+    <main className="flex flex-col max-w-full items-center justify-between pt-4 w:12/12">
       <div className="flex flex-col place-item-center">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] "
-          src="/logotipo.png"
+          src="/logotipo.jpeg"
           alt="Logo Aldo"
-          width={840}
+          width={540}
           height={480}
           priority
         />
@@ -40,7 +40,7 @@ export default function Home() {
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] "
           src="/mercadopago.png"
           alt="Logo Aldo"
-          width={840}
+          width={540}
           height={150}
           priority
         />
@@ -70,10 +70,10 @@ export default function Home() {
       <div className="flex flex-col text-center">
         <Image
           className="relative drop-shadow-[0_0_0.3rem_#ffffff70] "
-          src="/iceCream.jpg"
+          src="/muzzarella.jpeg"
           alt="Logo Aldo"
-          width={840}
-          height={180}
+          width={640}
+          height={80}
           priority
         />
       </div>
@@ -84,7 +84,7 @@ export default function Home() {
         </div>
         <div className="mx-4">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="py-1">
+            <div className="py-1 font-mono">
               <label htmlFor="firstName">Nombre: </label>
               <input
                 id="firstName"
@@ -100,14 +100,14 @@ export default function Home() {
               )}
             </div>
 
-            <div className="py-1">
+            <div className="py-1 font-mono">
               <label htmlFor="address">Direccion: </label>
               <input
                 id="address"
                 type="text"
                 {...register("address", {
                   required: true,
-                  maxLength: 80,
+                  maxLength: 120,
                   minLength: 2,
                 })}
               />
@@ -116,25 +116,28 @@ export default function Home() {
               )}
             </div>
 
-            <div className="py-1">
-              <label htmlFor="notes">Notas: </label>
+            <div className="py-1 font-mono">
+              <label htmlFor="details">Detalle: </label>
               <input
-                id="notes"
+                id="details"
                 type="text"
-                {...register("notes", {
+                {...register("details", {
                   required: true,
                   maxLength: 2,
                 })}
               />
-              {errors.notes && (
-                <span className="text-red-500"> Ingrese Notas</span>
+              {errors.details && (
+                <span className="text-red-500">
+                  {" "}
+                  Ingrese detalle del pedido.
+                </span>
               )}
             </div>
             <button
               className="mt-4 align-middle select-none font-sans font-bold uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg  bg-green-800 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none items-center gap-3"
               type="submit"
             >
-              <div className="m-1 flex items-center">
+              <div className="m-1 flex items-center font-extrabold hover:font-bold">
                 <WhatsappLogo /> {Label.DELIVERY_lABEL}
               </div>
             </button>
@@ -142,10 +145,10 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col text-center bg-slate-200 w-10/12 p-4">
+      <div className="flex flex-col text-center font-mono bg-slate-200 w-10/12 p-4">
         {Label.REGARDS_LABEL}
       </div>
-      <div className="flex flex-col  text-center bg-white p-2 w-10/12 text-black font-extrabold">
+      <div className="flex flex-col font-sans hover:font-bold text-center bg-grey-100 p-2 w-10/12 text-black font-extrabold">
         {Label.AUTHOR}
       </div>
     </main>
