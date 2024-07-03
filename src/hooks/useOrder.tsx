4830c717a8473c997c0d9fc76dd366e3f55e38c5
guillermogdv <guillermo.delvecchio@wegotcha.io>
2024-07-03 14:57:39 -0300

    remove unused useEffect

--- src/hooks/useOrder.tsx
+++ src/hooks/useOrder.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function useOrder() {
   const [order, setOrder] = useState({});
-
-  useEffect(() => {
-    console.log("order use effect", order);
-  }, [order]);
 
   return { order, setOrder };
 }
