db0c0794bffc57aefbc76f533dfab51471b36b52
guillermogdv <guillermo.delvecchio@wegotcha.io>
2024-07-03 21:59:35 -0300

    fix comma

--- src/components/orderForm/index.tsx
+++ src/components/orderForm/index.tsx
@@ -24,7 +24,7 @@ export default function OrderForm() {
     openInNewTab(
       `${
         Label.MESSAGE_DELIVERY
-      } Pedido a nombre de: ${firstName} Direccion: ${address} Pedido: ${JSON.stringify(
+      } Pedido a nombre de: ${firstName}, Direccion: ${address}, Pedido: ${JSON.stringify(
         cleanObject(data)
       )
         .replace(/"/g, "")
